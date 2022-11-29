ebd7624ff089311a3bf4a01542f651f6b8cda5f5
seanpden <seanden522@gmail.com>
2022-11-28 23:05:08 -0600

    comment, function cleaning, sim buttons implement

--- pages/events.py
+++ pages/events.py
@@ -5,6 +5,7 @@
 import streamlit as st
 import sqlalchemy
 import pandas as pd
+from datetime import datetime, timedelta
 
 # --- CONFIG/SETUP ---
 
@@ -26,9 +27,10 @@ col3, col4, col5, col6 = st.columns([1, 1, 1, 1])
 # --------------------
 
 # --- FUNCTIONS ---
+
+# db connection init
 cnxn_str = str('postgresql://Team5:team5@138.26.48.83/Team5DB')
 engine = sqlalchemy.create_engine(cnxn_str)
-# engine = engine.raw_connection()
 
 # @st.cache
 def get_water():
@@ -46,15 +48,37 @@ def get_events():
 def get_hvac():
     return pd.DataFrame(pd.read_sql(sql='SELECT * FROM hvac', con=engine)).sort_values(by=['time'])
 
+def bath_sim():
+    """Simulates a bath
+    """
+    now = datetime.now()
+    later = now+timedelta(minutes=+30)
+    engine.execute("INSERT INTO events (time, type, status) VALUES (%s, %s, %s)" % (now, "bath", True))
+    engine.execute("INSERT INTO events (time, type, status) VALUES (%s, %s, %s)" % (later, "bath", False))
+    engine.execute("INSERT INTO water (cost, time) VALUES (%s, %s)" % (.65*30, later))
+    engine.execute("INSERT INTO electric (cost, time) VALUES (%s, %s)", (4500/2, later))
+
+def microwave_sim():
+    """Simulates a shower
+    """
+    now = datetime.now()
+    later = now+timedelta(minutes=2)
+    engine.execute("INSERT INTO events (time, type, status) VALUES (%s, %s, %s)", now, "microwave", True)
+    engine.execute("INSERT INTO events (time, type, status) VALUES (%s, %s, %s)", later, "microwave", False)
+    engine.execute("INSERT INTO electric (cost, time) VALUES (%s, %s)", (1100/60)*2, later)
+
+
+
+
 # -----------------
 
 # --- PAGE CONTENT ---
 
 with col1:
-    st.button("Simulate Bath...", "bath_sim", "Demo: Simulates bath times")
+    st.button("Simulate Bath...", "bath_sim", "Demo: Simulates bath times", bath_sim)
 
 with col2:
-    st.button("Simulate Microwave...", 'microwave_sim', "Demo: Simuates microwave usage")
+    st.button("Simulate Microwave...", 'microwave_sim', "Demo: Simuates microwave usage", microwave_sim)
 
 with col3:
     st.subheader("HVAC table")
--- pages/metrics.py
+++ pages/metrics.py
@@ -5,8 +5,6 @@
 # TODO: MORE LABELS
 
 import streamlit as st
-import meteostat as ms
-from datetime import datetime
 import plotly.express as px
 import sqlalchemy
 import pandas as pd
@@ -32,14 +30,21 @@ col4, col5= st.columns([1,1])
 
 # --- FUNCTIONS ---
 
+# db connection init
 cnxn_str = str('postgresql://Team5:team5@138.26.48.83/Team5DB')
 engine = sqlalchemy.create_engine(cnxn_str)
 
-# @st.cache
+
 def get_water():
+    """gets water information from DB as dataframe, performs necessary transformations, returns agg sums
+
+    Returns:
+        DataFrame: Pandas Dataframe, grouped by month, summed for graphing
+    """
     # df[df.columns[1:]] = df[df.columns[1:]].replace('[\$,]', '', regex=True).astype(float)
     water = pd.DataFrame(pd.read_sql(sql='SELECT * FROM water', con=engine)).sort_values(by=['time']).drop(columns=['id'])
     # water['month'] = pd.DatetimeIndex(water['time']).month_name()
+    water['date'] = pd.DatetimeIndex(water['time']).date
     water['month'] = pd.DatetimeIndex(water['time']).month
     water['usage'] = water['cost'].replace('[\$,]', '', regex=True).astype(float)
     water['real_cost'] = (water['usage']*0.00336)
@@ -47,21 +52,25 @@ def get_water():
 
 # @st.cache
 def get_electric():
+    """gets electric information from db, performs necessary transformations, returns agg sums
+
+    Returns:
+        DataFrame: Pandas Dataframe, grouped by month, summed for graphing
+    """
     electric = pd.DataFrame(pd.read_sql(sql='SELECT * FROM electric', con=engine)).sort_values(by=['time']).drop(columns=['id'])
     # electric['month'] = pd.DatetimeIndex(electric['time']).month_name()
+    electric['date'] = pd.DatetimeIndex(electric['time']).date
     electric['month'] = pd.DatetimeIndex(electric['time']).month
     electric['usage'] = electric['cost'].replace('[\$,]', '', regex=True).astype(float)
     electric['real_cost'] = ((electric['usage']*(1/1000))*0.12)
     return electric.groupby(['month']).sum()
 
-# @st.cache
-def get_events(grouped=None, stat=None):
-    events = pd.DataFrame(pd.read_sql(sql='SELECT * FROM events', con=engine)).sort_values(by=['time'])
-    events['month'] = pd.DatetimeIndex(events['time']).month_name()
-    return events.groupby(['month'])
-
-# @st.cache
 def get_hvac():
+    """gets hvac information from db, performs necessary transforms, returns mean of monthly values
+
+    Returns:
+        _type_: _description_
+    """
     hvac = pd.DataFrame(pd.read_sql(sql='SELECT * FROM hvac', con=engine)).sort_values(by=['time'])
     hvac['month'] = pd.DatetimeIndex(hvac['time']).month
     return hvac.groupby(['month']).mean()
@@ -70,8 +79,12 @@ def get_hvac():
 
 # --- PAGE CONTENT ---
 
+# var inits
 water = get_water()
+elec = get_electric()
+hvac = get_hvac()
 
+# sidebar filtering
 st.sidebar.header("Filters here...")
 month = st.sidebar.multiselect(
     "Which month number to select?",
@@ -79,31 +92,34 @@ month = st.sidebar.multiselect(
     default=water.index[0]
 )
 
+# filtering implemented for water
 water_select = water.query(
     "month == @month"
 )
 
-elec = get_electric()
-
+# filtering implemented for electric
 elec_select = elec.query(
     "month == @month"
 )
 
-hvac = get_hvac()
-
+# col1, water scorecard
 with col1:
     st.metric(f"Water Cost: {month}", f"${(water_select['real_cost'].sum()):.2f}", f"Water usage (gallons): ~{water_select['usage'].sum():.0f}", delta_color="off")
 
+# col2, electric scorecard
 with col2:
     st.metric(f"Electric Cost: {month}", f"${(elec_select['real_cost'].sum()):.2f}", f"Electric usage (watts): ~{elec_select['usage'].sum():.0f}", delta_color="off")
 
+# col3, hvac graph
 with col3:
     # st.text("Interior/Exterior Farenheit vs. Month Number")
     st.write(px.line(hvac, x=hvac.index, y=[hvac['interiortemp'], hvac['exteriortemp']]).update_layout(title="Interior/Exterior temp vs. Time"))
 
+# col4, electric graph
 with col4:
     st.write(px.line(elec, x=elec.index, y=[elec['usage'], elec['real_cost']]).update_layout(title="Electricity usage and cost vs. Time"))
 
+# col5, water graph
 with col5:
     st.write(px.line(water, x=water.index, y=[water['usage'], water['real_cost']]).update_layout(title="Water usage and cost vs. Time"))
 
