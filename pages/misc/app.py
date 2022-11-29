4327ddbfaa76d7552e7f09243f177befe91339df
seanpden <seanden522@gmail.com>
2022-11-28 19:53:05 -0600

    Full push with venv

---
+++ pages/misc/app.py
@@ -0,0 +1,13 @@
+# import streamlit as st
+
+# from page_handler import MultiPage
+# from pages import page1, page2
+
+# app = MultiPage()
+
+# st.title('Test for multipage')
+
+# app.add_page("Page 1", page1.app)
+# app.add_page("Page 2", page2.app)
+
+# app.run()
